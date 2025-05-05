b681a7641020fde0041210beddd0ebf678cf9242
Ana Raquel de Holanda <anaraquel00@gmail.com>
2025-05-05 10:41:55 -0300

    first commit

---
+++ iPhone.java
@@ -0,0 +1,61 @@
+import interfaces.AparelhoTelefonico;
+import interfaces.NavegadorInternet;
+import interfaces.ReprodutorMusical;
+
+public class iPhone implements ReprodutorMusical, AparelhoTelefonico, NavegadorInternet {
+    private String modelo;
+    private int armazenamento;
+
+    public iPhone(String modelo, int armazenamento) {
+        this.modelo = modelo;
+        this.armazenamento = armazenamento;
+    }
+
+    // Implementações ReprodutorMusical
+    @Override
+    public void tocar() {
+        System.out.println("Tocando música...");
+    }
+
+    @Override
+    public void pausar() {
+        System.out.println("Música pausada");
+    }
+
+    @Override
+    public void selecionarMusica(String musica) {
+        System.out.println("Selecionando música: " + musica);
+    }
+
+    // Implementações AparelhoTelefonico
+    @Override
+    public void ligar(String numero) {
+        System.out.println("Ligando para: " + numero);
+    }
+
+    @Override
+    public void atender() {
+        System.out.println("Atendendo chamada...");
+    }
+
+    @Override
+    public void iniciarCorreioVoz() {
+        System.out.println("Iniciando correio de voz");
+    }
+
+    // Implementações NavegadorInternet
+    @Override
+    public void exibirPagina(String url) {
+        System.out.println("Exibindo página: " + url);
+    }
+
+    @Override
+    public void adicionarNovaAba() {
+        System.out.println("Nova aba adicionada");
+    }
+
+    @Override
+    public void atualizarPagina() {
+        System.out.println("Página atualizada");
+    }
+}
